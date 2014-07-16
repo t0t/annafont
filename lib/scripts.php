304e6e9517a50aaff343375e5a4a0f7b12e67524
Sergio Forés <sergiofores@gmail.com>
2014-07-16 09:59:58 +0200

    added mmenu and started to toquetear

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '67996e24df9fcaa947ff99248facd710');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e84f190bd4e59aa062d68cbdd2950656');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
