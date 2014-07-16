790876aea788b05f1026cccf3d0c41f4ca9ee009
Sergio Forés <sergiofores@gmail.com>
2014-07-17 00:35:02 +0200

    updated imported css url

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8a8d3c6149decdb1e403f44011fc2347');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c075c1a91597545dc2cbea397b13cdc8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
