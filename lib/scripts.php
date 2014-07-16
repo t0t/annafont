7811c2f4f11f96bea86115317c6bfc3947cfb0fc
Sergio Forés <sergiofores@gmail.com>
2014-07-16 11:08:54 +0200

    añadida variable paddingbase y sustituidas algunas variables

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e84f190bd4e59aa062d68cbdd2950656');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '35a4da2b2838919a02a4ee1d4fde444b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
