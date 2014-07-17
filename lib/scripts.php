8a0b92e8f83277ac77f4414686124db6653fdd1c
Sergio Forés <sergiofores@gmail.com>
2014-07-17 18:44:50 +0200

    cambios sin importancia

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c075c1a91597545dc2cbea397b13cdc8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ca787b81b824df369972fe979bcfd978');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
