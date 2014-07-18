31b585ec034d674beaef7735c2cd3bb00a148c3e
Sergio Forés <sergiofores@gmail.com>
2014-07-18 22:06:23 +0200

    centrado uls

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'afb18fbe321a411f56e6af20a193f61b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ca439964b75600428d4458a31c736913');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
