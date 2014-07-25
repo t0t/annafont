96192bba85af635235596cd683c0753ec4c686fb
Sergio Forés <sergiofores@gmail.com>
2014-07-25 02:13:48 +0200

    Menu adjustments and font

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a0f5e017da7be8d3be7769a4bf5aade1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0c3f188ff25f8fb4ed51d923fc1d4244');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
