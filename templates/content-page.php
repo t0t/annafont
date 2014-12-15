28eeea521314790ca3a7d6b06b5b476895b3c85f
Sergio Forés <sergiofores@gmail.com>
2014-12-15 17:29:04 +0100

    Arreglado div estrecho para ciertas pages

--- templates/content-page.php
+++ templates/content-page.php
@@ -7,16 +7,27 @@
     $images = get_field('image');
      
     if( $images ): ?>
+
         <ul class="gallery">
+
             <?php foreach( $images as $image ): ?>
+
                 <li class="gallery__item">
+
                 	<a href="<?php echo $image['url']; ?>">
+
                 		<img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" title="<?php echo $image['title']; ?>"/>
+                        
                 	</a>
+
                    <small><?php echo $image['caption']; ?></small>
+
                 </li>
+
             <?php endforeach; ?>
+
         </ul>
+
     <?php endif; ?>
 
 	<?php the_content(); ?>
